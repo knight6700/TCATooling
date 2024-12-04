4bc7d2d3d4cdf9bc4c728dcc3c513fa7c481adc9
Fares <knight6700@gmail.com>
2024-12-04 14:19:12 +0200

    add binding Reducer

---
+++ TCARootBinding.xctemplate/___VARIABLE_FEATURE_IDENTIFIER___Feature/___VARIABLE_FEATURE_IDENTIFIER___View.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+public struct ___VARIABLE_FEATURE_IDENTIFIER___Feature {
+    @ObservableState
+    public struct State: Equatable {
+        
+    }
+    
+    public enum Action: BindableAction, Equatable {
+        
+    }
+    
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce<State, Action>
+        { state, action in
+                .none
+        }
+    }
+}
+struct ___VARIABLE_FEATURE_IDENTIFIER___View: View {
+    
+    let store: StoreOf<___VARIABLE_FEATURE_IDENTIFIER___Feature>
+    
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+#if DEBUG
+#Preview {
+    ___VARIABLE_FEATURE_IDENTIFIER___View(
+        store: Store(
+            initialState: ___VARIABLE_FEATURE_IDENTIFIER___Feature.State(),
+            reducer: { ___VARIABLE_FEATURE_IDENTIFIER___Feature() }
+        )
+    )
+}
+#endif
